b7af80fefdd92a85a7fc8698a9f6ce5da4ac60f4
Mark Hall <mark.hall@work.room3b.eu>
2022-12-13 11:11:07 +0000

    Added extra UI controls for the local browser

--- src/widget.tsx
+++ src/widget.tsx
@@ -34,6 +34,20 @@ export class LocalBrowserWidget extends MainAreaWidget<IFrame> {
     });
     this.toolbar.addItem('ports', this._portsWidget);
 
+    this._pathWidget = new PathWidget({
+      onChange: () => {
+        this.toolbarChanged();
+      },
+      value: ''
+    });
+    this.toolbar.addItem('path', this._pathWidget);
+    const reloadButton = new ReloadWidget({
+      onClick: () => {
+        this.toolbarChanged();
+      }
+    });
+    this.toolbar.addItem('reload', reloadButton);
+
     this._statedb = options.statedb;
 
     this._serverSettings = ServerConnection.makeSettings();
@@ -47,6 +61,10 @@ export class LocalBrowserWidget extends MainAreaWidget<IFrame> {
     options.statedb.fetch(options.uuid).then((data: any) => {
       if (data) {
         this._portsWidget.value = data.port;
+        this._pathWidget.value =
+          data.pathname.charAt(0) === '/'
+            ? data.pathname.substring(1)
+            : data.pathname;
         const url =
           '/' +
           data.mode +
@@ -76,7 +94,17 @@ export class LocalBrowserWidget extends MainAreaWidget<IFrame> {
   }
 
   public toolbarChanged(): void {
-    this.content.url = '/proxy/' + this._portsWidget.value + '/';
+    if (this._portsWidget.value === '_placeholder') {
+      this.content.url = URLExt.join(
+        this._serverSettings.baseUrl,
+        'jupyterlab-local-browser',
+        'public',
+        'index.html'
+      );
+    } else {
+      this.content.url =
+        '/proxy/' + this._portsWidget.value + '/' + this._pathWidget.value;
+    }
   }
 
   protected onCloseRequest(msg: Message): void {
@@ -146,6 +174,7 @@ export class LocalBrowserWidget extends MainAreaWidget<IFrame> {
   private _loadPortsInterval = -1;
   private _statedb: IStateDB;
   private _portsWidget: SelectWidget;
+  private _pathWidget: PathWidget;
 }
 
 class SelectWidget extends ReactWidget {
@@ -192,6 +221,75 @@ class SelectWidget extends ReactWidget {
   private _values: [string, string][];
   private _value: string;
   private _onChange: () => void;
+}
+
+class PathWidget extends ReactWidget {
+  constructor(options: { onChange: () => void; value?: string }) {
+    super();
+
+    this._onChange = options.onChange;
+    this._value = options.value ? options.value : '';
+  }
+
+  get value(): string {
+    return this._value;
+  }
+
+  set value(value: string) {
+    this._value = value;
+    this.update();
+  }
+
+  onChange(evt: React.ChangeEvent<HTMLInputElement>) {
+    this._value = evt.target.value;
+    this._onChange();
+    this.update();
+  }
+
+  render(): JSX.Element {
+    return (
+      <input
+        type="text"
+        value={this._value}
+        onChange={evt => this.onChange(evt)}
+      ></input>
+    );
+  }
+
+  private _value: string;
+  private _onChange: () => void;
+}
+
+class ReloadWidget extends ReactWidget {
+  constructor(options: { onClick: () => void }) {
+    super();
+
+    this._onClick = options.onClick;
+  }
+
+  onClick() {
+    this._onClick();
+  }
+
+  render(): JSX.Element {
+    return (
+      <button
+        aria-label="Reload"
+        onClick={evt => {
+          this._onClick();
+        }}
+      >
+        <svg style={{ width: '16px', height: '16px' }} viewBox="0 0 24 24">
+          <path
+            fill="currentColor"
+            d="M2 12C2 16.97 6.03 21 11 21C13.39 21 15.68 20.06 17.4 18.4L15.9 16.9C14.63 18.25 12.86 19 11 19C4.76 19 1.64 11.46 6.05 7.05C10.46 2.64 18 5.77 18 12H15L19 16H19.1L23 12H20C20 7.03 15.97 3 11 3C6.03 3 2 7.03 2 12Z"
+          />
+        </svg>
+      </button>
+    );
+  }
+
+  private _onClick: () => void;
 }
 
 export namespace LocalBrowserWidget {
